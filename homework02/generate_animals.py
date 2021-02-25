237bef89fa3619cc7121da0dedb921f685ef99f8
Zoe Watson <zoeclairewatson@utexas.edu>
2021-02-25 05:45:04 -0600

    updated file for hw 2

---
+++ homework02/generate_animals.py
@@ -0,0 +1,36 @@
+import json
+
+import petname
+
+import random
+
+import sys
+
+def main():
+
+    animal_head_list = ['snake', 'bull', 'lion', 'raven', 'bunny']
+
+    bizarre_animals = {}
+    bizarre_animals['animals'] = []
+
+    for x in range(20):
+    
+        head = animal_head_list[random.randint(0, 4)]
+
+        body1 = petname.name()
+        body2 = petname.name()
+        body = body1 + '-' + body2
+
+        num_arms = random.randrange(2, 11, 2)
+
+        num_legs = random.randrange(3, 13, 3)
+
+        num_tails = num_arms + num_legs
+
+        bizarre_animals['animals'].append( {'head': head, 'body': body, 'arms': num_arms, 'legs': num_legs, 'tails': num_tails} )
+
+    with open('animals.json', 'w') as out:
+        json.dump(bizarre_animals, out, indent=2)
+
+if __name__ == '__main__':
+    main()
