18bc999ba252292eb4ea48e79f037e9b464a8691
Zoe Watson <zoeclairewatson@utexas.edu>
2021-02-25 07:48:29 -0600

    unit test format laid out

---
+++ homework02/test_read_animals.py
@@ -0,0 +1,14 @@
+import unittest
+
+from read_animals import new_functionality_breeding
+
+class TestReadAnimals(unittest.TestCase):
+
+    def test_new_functionality_breeding(self):
+        
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
