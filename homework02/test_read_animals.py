b4fb6d47178df1a457b051f32f882a4e5c4adb6b
Zoe Watson <zoeclairewatson@utexas.edu>
2021-02-25 10:45:44 -0600

    finalized

--- homework02/test_read_animals.py
+++ homework02/test_read_animals.py
@@ -6,8 +6,9 @@ class TestReadAnimals(unittest.TestCase):
 
     def test_new_functionality_breeding(self):
         
-
-
+        self.assertRaises(AssertionError, new_functionality_breeding, 4)
+        self.assertRaises(AssertionError, new_functionality_breeding, ['cat', 'dog'])
+        self.assertRaises(AssertionError, new_functionality_breeding, {'bunny', 'bull', 'snake'}
 
 
 if __name__ == '__main__':
